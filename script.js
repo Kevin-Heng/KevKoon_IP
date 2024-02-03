7756da8984fbc12a33887f8268e5bc592437bf8f
Kevin-Heng <s10258007@connect.np.edu.sg>
2024-02-03 16:22:13 +0800

    images change on click

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ function changeTrackFeature(){
     var collab_button = document.getElementById("collab-button");
     var name = document.getElementById("feature-name");
     var section = document.getElementById("main-desc");
+    var img = document.getElementById("feature-img");
 
     tracK_button.style.backgroundColor = "#009444";
     plan_button.style.backgroundColor='rgba(156, 156, 156,1)';
     collab_button.style.backgroundColor='rgba(156, 156, 156,1)';
     name.innerHTML = "<div>Track</div>";
-    section.innerHTML = "<div>Track your spendings, savings and financial goals</div>"
-    
+    section.innerHTML = "<div>Track your spendings, savings and financial goals</div>";
+
+    img.src = "blank.jpg"
+
+
 }
 
 function changePlanFeature(){
@@ -19,12 +23,15 @@ function changePlanFeature(){
     var collab_button = document.getElementById("collab-button");
     var name = document.getElementById("feature-name");
     var section = document.getElementById("main-desc");
+    var img = document.getElementById("feature-img");
 
     tracK_button.style.backgroundColor = "";
     plan_button.style.backgroundColor='';
     collab_button.style.backgroundColor='rgba(156, 156, 156,1)';
     name.innerHTML = "<div>Plan</div>";
-    section.innerHTML = "<div>Get down to the nitty gritty of planning your finance</div>"
+    section.innerHTML = "<div>Get down to the nitty gritty of planning your finance</div>";
+
+    img.src = "pikachu.png"
     
 }
 
@@ -34,11 +41,14 @@ function changeCollabFeature(){
     var collab_button = document.getElementById("collab-button");
     var name = document.getElementById("feature-name");
     var section = document.getElementById("main-desc");
+    var img = document.getElementById("feature-img");
 
     tracK_button.style.backgroundColor = "";
     plan_button.style.backgroundColor='rgba(156, 156, 156,1)';
     collab_button.style.backgroundColor='#009444';
     name.innerHTML = "<div>Collaborate</div>";
-    section.innerHTML = "<div>Add friends and collaborate with them in finanicial goals</div>"
+    section.innerHTML = "<div>Add friends and collaborate with them in finanicial goals</div>";
+
+    img.src = "jett.jpg"
     
 }
