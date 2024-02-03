9199d9410c6144ba22413b6706f6c3d4d4cb1811
Kevin-Heng <s10258007@connect.np.edu.sg>
2024-02-03 15:29:07 +0800

    change content on click of button

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-<script>
+function changeTrackFeature(){
+    var tracK_button = document.getElementById("track-button");
+    var plan_button = document.getElementById("plan-button");
+    var collab_button = document.getElementById("collab-button");
+    var name = document.getElementById("feature-name");
+
+    tracK_button.style.backgroundColor = "#009444";
+    plan_button.style.backgroundColor='rgba(156, 156, 156,1)';
+    collab_button.style.backgroundColor='rgba(156, 156, 156,1)';
+    name.innerHTML = "<div>Track</div>";
     
-</script>
+}
+
+function changePlanFeature(){
+    var tracK_button = document.getElementById("track-button");
+    var plan_button = document.getElementById("plan-button");
+    var collab_button = document.getElementById("collab-button");
+    var name = document.getElementById("feature-name");
+
+    tracK_button.style.backgroundColor = "";
+    plan_button.style.backgroundColor='';
+    collab_button.style.backgroundColor='rgba(156, 156, 156,1)';
+    name.innerHTML = "<div>Plan</div>";
+    
+}
+
+function changeCollabFeature(){
+    var tracK_button = document.getElementById("track-button");
+    var plan_button = document.getElementById("plan-button");
+    var collab_button = document.getElementById("collab-button");
+    var name = document.getElementById("feature-name");
+
+    tracK_button.style.backgroundColor = "";
+    plan_button.style.backgroundColor='rgba(156, 156, 156,1)';
+    collab_button.style.backgroundColor='#009444';
+    name.innerHTML = "<div>Collaborate</div>";
+    
+}
